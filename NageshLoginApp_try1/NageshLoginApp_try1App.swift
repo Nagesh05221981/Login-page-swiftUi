7129b9b633b38f06c847b86a8557286bb6f20437
Nagesh Allur <allur_nagesh@yahoo.co.in>
2025-11-22 16:06:27 -0600

    Initial Commit

---
+++ NageshLoginApp_try1/NageshLoginApp_try1App.swift
@@ -0,0 +1,17 @@
+//
+//  NageshLoginApp_try1App.swift
+//  NageshLoginApp_try1
+//
+//  Created by Nagesh Allur on 11/22/25.
+//
+
+import SwiftUI
+
+@main
+struct NageshLoginApp_try1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
