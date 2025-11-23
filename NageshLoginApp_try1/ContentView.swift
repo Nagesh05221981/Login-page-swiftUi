38d1f1ab7d990298432d7e260385cabed154bafd
Nagesh Allur <allur_nagesh@yahoo.co.in>
2025-11-23 09:30:20 -0600

    Initial commit

--- NageshLoginApp_try1/ContentView.swift
+++ NageshLoginApp_try1/ContentView.swift
@@ -8,14 +8,43 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var username = ""
+    @State private var password = ""
+    @State private var showLoginScreen = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationView{
+            
+            VStack{
+                Text("Login")
+                    .font(.largeTitle)
+                    .padding()
+                
+                TextField("Username",text:$username).padding(10).cornerRadius(20).padding(.horizontal).background(Color(.systemGray6))
+                SecureField("Password",text:$password).padding(10).cornerRadius(20).padding(.horizontal).background(Color(.systemGray6))
+                Button("Login"){
+                    //Authenticate User
+                    AuthenticateUser(username: username, password: password)
+                }.padding(10).cornerRadius(20).padding(.horizontal).background(Color(.systemGray6))
+                
+                NavigationLink(destination: Text("you are Loggedin"), isActive: $showLoginScreen){
+                    EmptyView()
+                    
+                }
+                
+            }
+            
+        }.navigationBarHidden(true)
+        
+    }
+    
+    func AuthenticateUser(username:String,password:String){
+        if username.lowercased() == "nagesh" && password == "1234"{
+            showLoginScreen=true
+        }else {
+            //wrong password
         }
-        .padding()
+        
     }
 }
 
